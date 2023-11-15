ca7623d96b07aacd6fbdb91951ad3754ee815d1c
Myles Traut <myles@linumlabs.com>
2023-11-15 11:58:52 +0200

    refactor

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -2,28 +2,24 @@ import { useAccount, useConnect, useDisconnect } from 'wagmi'
 import { InjectedConnector } from 'wagmi/connectors/injected'
  
 export default function Profile() {
-  const { address, isConnected } = useAccount()
+  const { isConnected } = useAccount();
   const { connect } = useConnect({
     connector: new InjectedConnector(),
-  })
-  const { disconnect } = useDisconnect()
+  });
+  const { disconnect } = useDisconnect();
  
-  if (isConnected)
     return (
       <>
-      <div className="flex relative justify-between items-center" >
-        
-        <button 
-        className="w-full h-12 px-6 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800"
-        onClick={() => disconnect()}>Disconnect</button>
-      </div>
+        <div className="flex relative items-center" >
+          {isConnected ? 
+          <button
+          className="w-full h-12 px-6 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800"
+          onClick={() => disconnect()}>Disconnect</button> :
+          <button 
+          className="w-full h-12 px-6 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800"
+          onClick={() => connect()}>Connect Wallet</button>
+          }
+        </div>
       </>
-    )
-  return (
-    <div className="flex relative items-center">
-        
-        <button className="w-full h-12 px-6 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800"
-        onClick={() => connect()}>Connect Wallet</button>
-    </div>
-  )
+    );
 }
